8da8af66904a1e6f3a18270627dcdff0faf59e58
bondhan bondhan <bondhan@sinbad.co.id>
2021-01-09 20:48:58 +0700

    reading xml files and parse to models

--- main.go
+++ main.go
@@ -4,14 +4,14 @@ import (
 	// "io/ioutil"
 
 	"encoding/json"
+	"encoding/xml"
 	"flag"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
-	"strings"
 
-	"github.com/beevik/etree"
 	"github.com/bondhan/xml2json/model"
 	"github.com/go-kit/kit/log"
 	"github.com/go-kit/kit/log/level"
@@ -20,147 +20,6 @@ import (
 var (
 	states map[string][]string
 )
-
-func check(logger log.Logger, err error) {
-	if err != nil {
-		logger.Log("Error", err)
-		os.Exit(-1)
-	}
-}
-
-func buildStates(root *etree.Element) {
-
-	states = make(map[string][]string)
-	for _, rpminfoState := range root.FindElements("//rpminfo_state") {
-		if id := rpminfoState.SelectAttr("id"); id != nil {
-			states[id.Value] = []string{}
-			if arch := rpminfoState.SelectElement("arch"); arch != nil {
-				if opr := arch.SelectAttr("operation"); opr != nil {
-					states[id.Value] = append(states[id.Value], opr.Value, "arch")
-				}
-			}
-
-			if evr := rpminfoState.SelectElement("evr"); evr != nil {
-				states[id.Value] = append(states[id.Value], evr.Text())
-				if dtType := evr.SelectAttr("datatype"); dtType != nil {
-					states[id.Value] = append(states[id.Value], dtType.Value)
-					if evrOpr := evr.SelectAttr("operation"); evrOpr != nil {
-						states[id.Value] = append(states[id.Value], evrOpr.Value, "evr")
-					}
-				}
-			}
-
-			if sign := rpminfoState.SelectElement("signature_keyid"); sign != nil {
-				states[id.Value] = append(states[id.Value], sign.Text())
-				if opr := sign.SelectAttr("operation"); opr != nil {
-					states[id.Value] = append(states[id.Value], opr.Value, "signature_keyid")
-				}
-			}
-
-		}
-	}
-
-	for _, rpminfoState := range root.FindElements("//rpmverifyfile_state") {
-		if id := rpminfoState.SelectAttr("id"); id != nil {
-			states[id.Value] = []string{}
-			if name := rpminfoState.SelectElement("name"); name != nil {
-				states[id.Value] = append(states[id.Value], name.Text())
-				if opr := name.SelectAttr("operation"); opr != nil {
-					states[id.Value] = append(states[id.Value], opr.Value)
-				}
-			}
-
-			if ver := rpminfoState.SelectElement("version"); ver != nil {
-				states[id.Value] = append(states[id.Value], ver.Text())
-				if opr := ver.SelectAttr("operation"); opr != nil {
-					states[id.Value] = append(states[id.Value], opr.Value)
-				}
-			}
-
-		}
-	}
-
-	for id, val := range states {
-		fmt.Println("id:", id)
-		fmt.Println("properties:")
-		for _, s := range val {
-			fmt.Println("        ", s)
-		}
-	}
-	fmt.Println()
-
-}
-
-func parseCriterion(crit *etree.Element) []string {
-	var criteon []string
-	for _, criterion := range crit.SelectElements("criterion") {
-		if testRef := criterion.SelectAttr("test_ref"); testRef != nil {
-			fmt.Printf("test_ref: %s\n", testRef.Value)
-			criteon = append(states[testRef.Value])
-		}
-		if comment := criterion.SelectAttr("comment"); comment != nil {
-			fmt.Printf("comment: %s\n", comment.Value)
-		}
-	}
-
-	return criteon
-}
-
-func recParseCriteria(parent *etree.Element, mp []model.UnitData) string {
-
-	for _, crit := range parent.SelectElements("criteria") {
-		if opr := crit.SelectAttr("operator"); opr != nil {
-			if strings.EqualFold(opr.Value, "OR") || strings.EqualFold(opr.Value, "AND") {
-
-				fmt.Printf("operator: %s\n", opr.Value)
-
-				ret := parseCriterion(crit)
-				newMap := make(map[string][]string)
-
-				if ret != nil && len(ret) > 0 {
-					newMap[opr.Value] = ret
-					mp = append(mp, newMap)
-				}
-
-				recParseCriteria(crit, mp)
-			}
-		}
-	}
-	//walk through the criteria  and when matching the states, insert the state according to id
-
-	fmt.Println()
-	return ""
-}
-
-func parseCriteria(root *etree.Element) []model.UnitData {
-
-	m := []model.UnitData{}
-
-	defs := root.SelectElement("definitions")
-	def := defs.SelectElement("definition")
-	_ = def
-	for _, crit := range def.SelectElements("criteria") {
-		if opr := crit.SelectAttr("operator"); opr != nil {
-			if strings.EqualFold(opr.Value, "OR") || strings.EqualFold(opr.Value, "AND") {
-				fmt.Printf("operator: %s\n", opr.Value)
-
-				ret := parseCriterion(crit)
-				newMap := make(map[string][]string)
-
-				if ret != nil && len(ret) > 0 {
-					newMap[opr.Value] = ret
-					m = append(m, newMap)
-				}
-
-				recParseCriteria(crit, m)
-			}
-		}
-	}
-	//walk through the criteria  and when matching the states, insert the state according to id
-
-	fmt.Println()
-	return m
-}
 
 func main() {
 
@@ -199,61 +58,25 @@ func main() {
 		os.Exit(-1)
 	}
 
-	doc := etree.NewDocument()
-	if err := doc.ReadFromFile("file/input.xml"); err != nil {
-		panic(err)
+	xmlFile, err := os.Open("file/input.xml")
+	// if we os.Open returns an error then handle it
+	if err != nil {
+		logger.Log("Error", err)
+	}
+	defer xmlFile.Close()
+
+	byteValue, _ := ioutil.ReadAll(xmlFile)
+
+	// we initialize our Users array
+	var root model.Oval
+	err = xml.Unmarshal(byteValue, &root)
+	if err != nil {
+		logger.Log("Error", err)
 	}
 
-	AdvisoryDetails := model.AdvisoryDetails{}
-	Title := model.Title{
-		Title: "...",
+	m, err := json.Marshal(root)
+	if err != nil {
+		logger.Log("Error", err)
 	}
-
-	root := doc.SelectElement("oval_definitions")
-
-	//title
-	for _, md := range root.FindElements("//metadata") {
-		AdvisoryDetails.Title = md.SelectElement("title").Text()
-	}
-
-	//reference cve
-	var cves []string
-	for _, ref := range root.FindElements("//reference") {
-		if src := ref.SelectAttr("source"); src != nil {
-			if strings.EqualFold(src.Value, "CVE") {
-
-				if refid := ref.SelectAttr("ref_id"); refid != nil {
-					cves = append(cves, refid.Value)
-				}
-			}
-		}
-	}
-	AdvisoryDetails.FixesCVE = append(AdvisoryDetails.FixesCVE, cves...)
-
-	//severity
-	if svrty := root.FindElement("//severity"); svrty != nil {
-		AdvisoryDetails.Severity = svrty.Text()
-	}
-
-	//cpe_list
-	var cpes []string
-	if cpeList := root.FindElement("//affected_cpe_list"); cpeList != nil {
-		for _, cpe := range cpeList.FindElements("cpe") {
-			cpes = append(cpes, cpe.Text())
-		}
-	}
-
-	AdvisoryDetails.AffectedCpe = append(AdvisoryDetails.AffectedCpe, cpes...)
-
-	buildStates(root)
-	m := parseCriteria(root)
-	AdvisoryDetails.Criteria = m
-
-	rhJSONModel := make(map[string][]interface{})
-
-	rhJSONModel["advisory"] = append(rhJSONModel["advisory"], AdvisoryDetails, Title)
-
-	obj, err := json.Marshal(rhJSONModel)
-	check(logger, err)
-	fmt.Println(string(obj))
+	fmt.Printf("\n\nResult: \n%s\n", m)
 }
--- model/model.go
+++ model/model.go
@@ -1,15 +1,148 @@
 package model
 
-type UnitData map[string][]string
+import "encoding/xml"
 
-type AdvisoryDetails struct {
-	Title       string     `json:"title"`
-	FixesCVE    []string   `json:"fixes_cve"`
-	Severity    string     `json:"severity"`
-	AffectedCpe []string   `json:"affected_cpe"`
-	Criteria    []UnitData `json:"criteria"`
+//Oval ...
+type Oval struct {
+	// XMLName           xml.Name    `xml:"oval_definitions" json:"ovalDefinitions"`
+	Xmlns             string      `xml:"xmlns,attr" json:"xmlns"`
+	XmlnsOval         string      `xml:"oval,attr" json:"oval"`
+	XmlnsOvalDef      string      `xml:"oval-def,attr" json:"ovalDef"`
+	XmlnsUnixDef      string      `xml:"unix-def,attr" json:"unixDef"`
+	XmlnsRedDef       string      `xml:"red-def,attr" json:"redDef"`
+	XmlnsXsi          string      `xml:"xsi,attr" json:"xsi"`
+	XsiSchemaLocation string      `xml:"schemaLocation,attr" json:"schemaLocations"`
+	Generator         generator   `xml:"generator" json:"generator"`
+	Definitions       definitions `xml:"definitions" json:"definitions"`
+	Tests             tests       `xml:"tests" json:"tests"`
+	Objects           objects     `xml:"objects" json:"objects"`
+	// States            states      `xml:"states" json:"states"`
 }
 
-type Title struct {
-	Title string `json:"title"`
+type generator struct {
+	// XMLName           xml.Name `xml:"generator" json:"generator"`
+	OvalProductName   string `xml:"product_name" json:"productName"`
+	OvalSchemaVersion string `xml:"schema_version" json:"schemaVersion"`
+	OvalTimestamp     string `xml:"timestamp" json:"timestamp"`
+}
+
+type definitions struct {
+	Definition definition `xml:"definition" json:"definition"`
+}
+
+type definition struct {
+	// XMLName xml.Name `xml:"definition" json:"definition"`
+	Class    string   `xml:"class,attr" json:"class"`
+	ID       string   `xml:"id,attr" json:"id"`
+	Version  string   `xml:"version,attr" json:"version"`
+	Metadata metadata `xml:"metadata" json:"metadata"`
+	Criteria criteria `xml:"criteria" json:"criteria,omitempty"`
+}
+
+type metadata struct {
+	// XMLName     xml.Name    `xml:"metadata" json:"metadata"`
+	Title       string      `xml:"title" json:"title"`
+	Affected    affected    `xml:"affected" json:"affected"`
+	Reference   []reference `xml:"reference" json:"reference"`
+	Description string      `xml:"description" json:"description"`
+	Advisory    advisory    `xml:"advisory" json:"advisory"`
+}
+
+type affected struct {
+	// XMLName  xml.Name `xml:"affected" json:"affected"`
+	Family   string   `xml:"family,attr" json:"family"`
+	Platform []string `xml:"platform" json:"platform"`
+}
+
+type reference struct {
+	XMLName xml.Name `xml:"reference" json:"reference"`
+	RefID   string   `xml:"ref_id,attr" json:"refId"`
+	RefURL  string   `xml:"ref_url,attr" json:"refUrl"`
+	Source  string   `xml:"source,attr" json:"source"`
+}
+type advisory struct {
+	// XMLName     xml.Name `xml:"advisory" json:"advisory"`
+	From     string  `xml:"from,attr" json:"from"`
+	Severity string  `xml:"severity" json:"severity"`
+	Rights   string  `xml:"rights" json:"rights"`
+	Issued   issued  `xml:"issued" json:"issued"`
+	Updated  updated `xml:"updated" json:"updated"`
+}
+
+type issued struct {
+	IssuedDate string `xml:"date,attr" json:"date"`
+}
+
+type updated struct {
+	UpdatedDate string `xml:"date,attr" json:"date"`
+}
+
+type criteria struct {
+	// XMLName   xml.Name   `xml:"criteria"`
+	CriteriaOperator string      `xml:"operator,attr" json:"operator"`
+	Criterion        []criterion `xml:"criterion" json:"criterion,omitempty"`
+	Criteria         []criteria  `xml:"criteria" json:"criteria,omitempty"`
+}
+
+type criterion struct {
+	Comment string `xml:"comment,attr" json:"comment"`
+	TestRef string `xml:"test_ref,attr" json:"testRef"`
+}
+
+type tests struct {
+	// XMLName   xml.Name `xml:"tests"`
+	RpmInfo   []rpminfo   `xml:"rpminfo_test" json:"rpmInfoTest"`
+	RpmVerify []rpmverify `xml:"rpmverifyfile_test" json:"rpmVerifyFileTest"`
+}
+
+type rpminfo struct {
+	Xmlns   string `xml:"xmlns,attr" json:"xmlns"`
+	Check   string `xml:"check,attr" json:"check"`
+	Comment string `xml:"comment,attr" json:"comment"`
+	ID      string `xml:"id,attr" json:"id"`
+	Version string `xml:"version,attr" json:"version"`
+
+	Object obj   `xml:"object" json:"object"`
+	State  state `xml:"state" json:"state"`
+}
+
+type rpmverify struct {
+	Xmlns   string `xml:"xmlns,attr" json:"xmlns"`
+	Check   string `xml:"check,attr" json:"check"`
+	Comment string `xml:"comment,attr" json:"comment"`
+	ID      string `xml:"id,attr" json:"id"`
+	Version string `xml:"version,attr" json:"version"`
+
+	Object obj   `xml:"object" json:"object"`
+	State  state `xml:"state" json:"state"`
+}
+
+type obj struct {
+	ObjectRef string `xml:"object_ref,attr" json:"objectRef"`
+}
+
+type state struct {
+	StateRef string `xml:"state_ref,attr" json:"stateRef"`
+}
+
+type objects struct {
+	// XMLName       xml.Name `xml:"objects"`
+	RpmInfoObject   []rpminfoobject   `xml:"rpminfo_object"`
+	RpmVerifyObject []rpmverifyobject `xml:"rpmverifyfile_object"`
+}
+
+type rpminfoobject struct {
+	Xmlns   string `xml:"xmlns,attr" json:"xmlns"`
+	ID      string `xml:"id,attr" json:"id"`
+	Version string `xml:"version,attr" json:"version"`
+	Name    string `xml:"name" json:"name"`
+}
+type rpmverifyobject struct {
+	Xmlns   string `xml:"xmlns,attr" json:"xmlns"`
+	ID      string `xml:"id,attr" json:"id"`
+	Version string `xml:"version,attr" json:"version"`
+}
+
+type states struct {
+	// XMLName xml.Name `xml:"states"`
 }
